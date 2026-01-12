bb4014505ceff17c7978156291801cd6a3957358
DmytroLavrov <dimalavrov2004.06.16@gmail.com>
2026-01-13 01:38:27 +0200

    chore(app): setup router with lazy-loaded login and board pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./pages/login/login.component').then((m) => m.LoginComponent),
+  },
+  {
+    path: 'board',
+    loadComponent: () =>
+      import('./pages/board/board.component').then((m) => m.BoardComponent),
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+];
---
+++ src/app/pages/board/board.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-board',
+  imports: [],
+  templateUrl: './board.component.html',
+  styleUrl: './board.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class BoardComponent {
+
+}
